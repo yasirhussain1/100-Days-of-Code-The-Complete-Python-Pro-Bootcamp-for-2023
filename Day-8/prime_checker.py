5d4b98558feae84c0580318d198dc5f2d22ca3d0
yasirhussain1 <30301982+yasirhussain1@users.noreply.github.com>
2023-04-22 18:04:10 -0700

    Create prime_checker.py

---
+++ Day-8/prime_checker.py
@@ -0,0 +1,36 @@
+#Write your code below this line 👇
+
+def prime_checker(number):
+    for i in range(2, number):
+        if number % i == 0:
+            print("It's not a prime number.")
+            break
+    else:
+        print("It's a prime number.")
+
+
+#Write your code above this line 👆
+    
+#Do NOT change any of the code below👇
+n = int(input("Check this number: "))
+prime_checker(number=n)
+
+# Another way to write the same program
+
+#Write your code below this line 👇
+
+def prime_checker(number):
+    is_prime = True
+    for i in range(2, number):
+        if number % i == 0:
+            is_prime = False
+    if is_prime:
+      print("It's a prime number")
+    else:
+      print("It's not a prime number.")
+
+#Write your code above this line 👆
+    
+#Do NOT change any of the code below👇
+n = int(input("Check this number: "))
+prime_checker(number=n)
